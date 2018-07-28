968376d1e9c3c47beeb0ba1eaf535542d51afbe4
Orenday <orendaym@github.com>
2018-07-28 00:29:06 -0500

    Nada

---
+++ js/general.js
@@ -0,0 +1,14 @@
+/*$('#pL').click(function(event) {
+	event.preventDefault();
+	document.location.href="angular/angular.html";
+});
+
+$('#pA').click(function(event) {
+	event.preventDefault();
+	document.location.href="/proyectos-angular";
+});
+
+$('#pB').click(function(event) {
+	event.preventDefault();
+	document.location.href="/proyectos-bootstrap";
+});*/
