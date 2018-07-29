08ab3c024c39e38b6eeef9fcc982b1a79ba6f886
Orenday <orendaym@github.com>
2018-07-29 13:25:44 -0500

    Añadido efecto de desplazamiento entre secciones

--- js/general.js
+++ js/general.js
@@ -1,14 +1,27 @@
-/*$('#pL').click(function(event) {
-	event.preventDefault();
-	document.location.href="angular/angular.html";
-});
 
-$('#pA').click(function(event) {
-	event.preventDefault();
-	document.location.href="/proyectos-angular";
-});
 
-$('#pB').click(function(event) {
-	event.preventDefault();
-	document.location.href="/proyectos-bootstrap";
-});*/
+
+$(function(){ //Desplazamiento suave
+
+     $('a[href*=\\#]').click(function(evt) {
+      evt.preventDefault();
+     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+             var $target = $(this.hash);
+
+             $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+
+             if ($target.length) {
+
+                 var targetOffset = $target.offset().top-60; //Se posiciona el scroll en el top + 60 px de margen top que contiene la barra de navegación 
+
+                 $('html,body').animate({scrollTop: targetOffset}, 600); //velocidad de animación y distancia de desplazamiento
+
+                
+            }
+
+       }
+
+   });
+
+});
